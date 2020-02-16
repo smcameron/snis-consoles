183304fbe2288eefff3767cbca9ed15ba3e45914
Stephen M. Cameron <stephenmcameron@gmail.com>
2020-02-15 21:44:03 -0500

    Add beginnings of code to update indicator LEDs

--- src/snis_console.c/snis_console.c.ino
+++ src/snis_console.c/snis_console.c.ino
@@ -21,15 +21,23 @@
 
 /* Multiplexer selector pins */
 static const int mux_select[4] = { 4, 5, 6, 7 };
-static const int mux_data[2] = { A2, A3 };
+static const int mux_input[2] = { A2, A3 };
+
+/* Shift register control pins */
+static const int sr_data = 8;
+static const int sr_clock = 9;
+static const int sr_latch = 10;
 
 /* Changes below +/- 3 won't register... cuts down on spurious triggers
  * Might be able to tighten this up when no longer using the breadboard
  * when the real circuit is built. */
 #define MUX_SIGNAL_THRESHOLD 3
 
-static int current_data[32] = { 0 };
-static int old_data[32] = { 0 };
+static int current_input[32] = { 0 };
+static int old_input[32] = { 0 };
+static int indicator_led[16] = { 0 };
+
+
 
 #define ENGINEERING 1
 
@@ -61,7 +69,14 @@ void setup()
 	/* Set up output pins for mux channel selection */
 	for (i = 0; i < 4; i++)
 		pinMode(mux_select[i], OUTPUT);
-
+	pinMode(mux_input[0], INPUT);
+	pinMode(mux_input[1], INPUT);
+	pinMode(sr_data, OUTPUT);
+	pinMode(sr_clock, OUTPUT);
+	pinMode(sr_latch, OUTPUT);
+	digitalWrite(sr_data, LOW);
+	digitalWrite(sr_latch, LOW);
+	digitalWrite(sr_clock, LOW);
 }
 
 void select_mux_channel(int channel)
@@ -70,16 +85,16 @@ void select_mux_channel(int channel)
 		digitalWrite(mux_select[i], (channel >> i) & 0x01);
 }
 
-void read_mux_data(int current_data[])
+void read_mux_input(int current_input[])
 {
 	for (int i = 0; i < 16; i++) {
 		select_mux_channel(i);
-		current_data[i] = analogRead(mux_data[0]);
-		current_data[i + 16] = analogRead(mux_data[1]);
+		current_input[i] = analogRead(mux_input[0]);
+		current_input[i + 16] = analogRead(mux_input[1]);
 	}
 }
 
-void transmit_data(int datum, int value)
+void transmit_input(int datum, int value)
 {
 	Serial.print(CONSOLE_CHAR);
 	Serial.print(datum);
@@ -87,20 +102,40 @@ void transmit_data(int datum, int value)
 	Serial.println(value);
 }
 
-void transmit_changed_data(int current_data[], int old_data[])
+void transmit_changed_input(int current_input[], int old_input[])
 {
 	int i;
 
 	for (i = 0; i < 32; i++) {
-		if (abs(current_data[i] - old_data[i]) > MUX_SIGNAL_THRESHOLD) {
-			transmit_data(i, current_data[i]);
-			old_data[i] = current_data[i];
+		if (abs(current_input[i] - old_input[i]) > MUX_SIGNAL_THRESHOLD) {
+			transmit_input(i, current_input[i]);
+			old_input[i] = current_input[i];
 		}
 	}
 }
 
+void read_indicator_data_from_snis_client(void)
+{
+	/* TODO: Read indicator data from snis_client, if any */
+}
+
+void update_indicator_leds(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++) { /* Shift out 16 bits of LED indicator data to turn on/off 16 indicator LEDs */
+		digitalWrite(sr_clock, LOW);
+		digitalWrite(sr_data, indicator_led[i] ? HIGH : LOW);
+		digitalWrite(sr_clock, HIGH);
+	}
+	digitalWrite(sr_latch, HIGH); /* Latch the data so it appears on the outputs of the shift registers */
+	digitalWrite(sr_latch, LOW);
+}
+
 void loop() {
 
-	read_mux_data(current_data);
-	transmit_changed_data(current_data, old_data);
+	read_mux_input(current_input);
+	transmit_changed_input(current_input, old_input);
+	read_indicator_data_from_snis_client();
+	update_indicator_leds();
 }
